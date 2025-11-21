9dbda303273576ef5fd5e0d8bea595b67419418a
jcode82 <jflifestyles@gmail.com>
2025-11-21 00:02:33 -0500

    [LAB-51]feat: add heading anchor links to MDX articles

--- mdx.config.mjs
+++ mdx.config.mjs
@@ -1,7 +1,21 @@
 import rehypePrettyCode from "rehype-pretty-code";
 import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 export default {
   remarkPlugins: [remarkGfm],
-  rehypePlugins: [[rehypePrettyCode, { theme: "github-dark" }]],
+  rehypePlugins: [
+    [rehypePrettyCode, { theme: "github-dark" }],
+    rehypeSlug,
+    [
+      rehypeAutolinkHeadings,
+      {
+        behavior: "append",
+        properties: {
+          className: ["anchor-link"],
+        },
+      },
+    ],
+  ],
 };
